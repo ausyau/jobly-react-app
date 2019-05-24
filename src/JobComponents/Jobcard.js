172e32a6227c21670b747459b17214236f349245
Austin <austin.yau@gmail.com>
2019-05-24 11:51:51 -0700

    css and bug fix

--- src/JobComponents/Jobcard.js
+++ src/JobComponents/Jobcard.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 export default class Jobcard extends Component {
   constructor(props) {
     super(props);
+
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -14,14 +15,16 @@ export default class Jobcard extends Component {
   }
 
   render() {
-
     let applyButton = <button type="button" className="btn btn-danger" onClick={this.handleClick} >Apply </button>;
     let appliedButton = <button type="button" className="btn btn-danger" disabled> Applied </button>;
 
-    let isApplied = this.props.user.jobs.some(job => job.id === this.props.id);
+    let isApplied = [];
+    if(this.props.user.jobs){
+      isApplied = this.props.user.jobs.some(job => job.id === this.props.id);
+    }
 
     return (
-      <div className="card">
+      <div className="card mt-3 col-9 offset-md-2">
         <div className="card-body" id={this.props.id}>
           <h5 className="card-title">{this.props.title}</h5>
           <p className="card-text">Salary: {this.props.salary} Equity: {this.props.equity}</p>
--- src/ProfileComponents/Home.js
+++ src/ProfileComponents/Home.js
@@ -4,12 +4,10 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        <div className="home-bg">
-          <div className="jumbotron jumbotron-fluid">
-            <div className="container">
-              <h1 className="display-4">Welcome to Jobly</h1>
-              <p className="lead">Find some jobs and register</p>
-            </div>
+        <div className="jumbotron jumbotron-fluid home-jumbo mx-auto">
+          <div className="container">
+            <h1 className="display-4">Welcome to Jobly</h1>
+            <p className="lead">Find some jobs and register</p>
           </div>
         </div>
       </div>
